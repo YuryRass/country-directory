7c6bffd409ffd57359c178874b4fc04a85b7aaba
YuryRass <superuser@mail.ru>
2024-06-10 16:52:22 +0300

    used pydantic-dotenv

--- src/settings.py
+++ src/settings.py
@@ -2,28 +2,33 @@
 Настройки проекта.
 """
 
-import os
-from typing import Optional
 
-# путь к директории для сохранения файлов
-MEDIA_PATH: str = os.getenv("MEDIA_PATH", "../media")
+from pydantic import BaseSettings
 
-# путь к директории для логирования
-LOGGING_PATH: str = os.getenv("LOGGING_PATH", "../logs")
-# формат для записей логов
-LOGGING_FORMAT: str = os.getenv(
-    "LOGGING_FORMAT", "%(name)s %(asctime)s %(levelname)s %(message)s"
-)
-# уровень логирования
-LOGGING_LEVEL: str = os.getenv("LOGGING_LEVEL", "INFO")
 
-# ключи для доступа к API
-API_KEY_APILAYER: Optional[str] = os.getenv("API_KEY_APILAYER")
-API_KEY_OPENWEATHER: Optional[str] = os.getenv("API_KEY_OPENWEATHER")
+class Settings(BaseSettings):
 
-# время актуальности данных о странах (в секундах), по умолчанию – один год
-CACHE_TTL_COUNTRY: int = int(os.getenv("CACHE_TTL_COUNTRY", "31_536_000"))
-# время актуальности данных о курсах валют (в секундах), по умолчанию – сутки
-CACHE_TTL_CURRENCY_RATES: int = int(os.getenv("CACHE_TTL_CURRENCY_RATES", "86_400"))
-# время актуальности данных о погоде (в секундах), по умолчанию ~ три часа
-CACHE_TTL_WEATHER: int = int(os.getenv("CACHE_TTL_WEATHER", "10_700"))
+    # путь к директории для сохранения файлов
+    MEDIA_PATH: str = "../media"
+
+    # путь к директории для логирования
+    LOGGING_PATH: str = "../logs"
+    # формат для записей логов
+    LOGGING_FORMAT: str = "%(name)s %(asctime)s %(levelname)s %(message)s"
+
+    # уровень логирования
+    LOGGING_LEVEL: str = "INFO"
+
+    # ключи для доступа к API
+    API_KEY_APILAYER: str
+    API_KEY_OPENWEATHER: str
+
+    # время актуальности данных о странах (в секундах), по умолчанию – один год
+    CACHE_TTL_COUNTRY: int = int("31_536_000")
+    # время актуальности данных о курсах валют (в секундах), по умолчанию – сутки
+    CACHE_TTL_CURRENCY_RATES: int = int("86_400")
+    # время актуальности данных о погоде (в секундах), по умолчанию ~ три часа
+    CACHE_TTL_WEATHER: int = int("10_700")
+
+
+settings = Settings(_env_file=".env")
