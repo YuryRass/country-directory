260317ce8ef7dd9565c19a5bd102af22b7c2aeb5
YuryRass <yurchik1997yurchik@gmail.com>
2024-06-13 16:01:03 +0300

    added short country names

--- src/clients/news.py
+++ src/clients/news.py
@@ -11,6 +11,15 @@ from clients.base import BaseClient
 from logger import trace_config
 from settings import settings
 
+COUNTRY_SHORT_NAMES = [
+    "ae", "ar", "at", "au", "be", "bg", "br", "ca", "ch",
+    "cn", "co", "cu", "cz", "de", "eg", "fr", "gb","gr", "hk",
+    "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv",
+    "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt",
+    "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr",
+    "tw", "ua", "us", "ve", "za",
+]
+
 
 class NewsClient(BaseClient):
     """
