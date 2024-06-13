e1821b39f9bf30f6e8fdb49f8ea01815894c1cde
YuryRass <superuser@mail.ru>
2024-06-13 13:23:42 +0300

    added API_KEY_NEWS and CACHE_TTL_NEWS

--- src/settings.py
+++ src/settings.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
     # ключи для доступа к API
     API_KEY_APILAYER: str
     API_KEY_OPENWEATHER: str
+    API_KEY_NEWS: str
 
     # время актуальности данных о странах (в секундах), по умолчанию – один год
     CACHE_TTL_COUNTRY: int = int("31_536_000")
@@ -29,6 +30,8 @@ class Settings(BaseSettings):
     CACHE_TTL_CURRENCY_RATES: int = int("86_400")
     # время актуальности данных о погоде (в секундах), по умолчанию ~ три часа
     CACHE_TTL_WEATHER: int = int("10_700")
+    # время актуаьности новостей о странах (в секундах), по умолчанию - 1 час
+    CACHE_TTL_NEWS: int = 3600
 
 
 settings = Settings(_env_file="../.env")
