14c9d8e4678d8171fa2997d140941a1cce9242e5
YuryRass <yurchik1997yurchik@gmail.com>
2024-06-14 09:47:15 +0300

    updated

--- src/tests/clients/test_weather.py
+++ src/tests/clients/test_weather.py
@@ -1,3 +1,31 @@
 """
 Тестирование функций клиента для получения информации о погоде.
 """
+
+import pytest
+
+from clients.weather import WeatherClient
+from settings import settings
+
+
+@pytest.mark.asyncio
+class TestClientWeather:
+    """
+    Тестирование клиента для получения информации о погоде.
+    """
+
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+    @pytest.fixture
+    def client(self):
+        return WeatherClient()
+
+    async def test_get_base_url(self, client: WeatherClient):
+        assert await client.get_base_url() == self.base_url
+
+    async def test_get_rates(self, mocker, client: WeatherClient):
+        mocker.patch("clients.weather.WeatherClient._request")
+        await client.get_weather("china")
+        client._request.assert_called_once_with(
+            f"{self.base_url}?units=metric&q=china&appid={settings.API_KEY_OPENWEATHER}"
+        )
